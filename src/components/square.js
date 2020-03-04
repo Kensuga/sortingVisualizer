8bc2d5219716f09bb59fdaa6b0c7b83d49a8828d
Kensuga <cruzlmcastillo@gmail.com>
2020-03-03 21:36:51 -0800

    Minor Changes. Working on Merge Sort.

--- src/components/square.js
+++ src/components/square.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
 export default class square extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      height: this.setSize()
+    }
+  }
     setSize(){
         if(this.props.value > 0){
-        return (this.props.value)
+        return ((this.props.value/this.props.length)*100)
         } 
-        if(this.props.value === 0){
-            return 1
-        }
     }
 
   render(){
   return (
-    <div style={{ height: this.setSize(), width: 1, backgroundColor: "teal" }}></div>
+    <div style={{ height: `${this.setSize()}%`, width: 1, backgroundColor: "teal" }}></div>
   );
   }
 }
--- src/components/visualizer.js
+++ src/components/visualizer.js
@@ -11,13 +11,83 @@ class Visualizer extends Component {
 
     createArray() {
         //Change Array Value for width
-        let array = Array(1750).fill()
+        let array = Array(1000).fill()
         // Populates the array with numbers
         for(let i = 0; i < array.length; i++){
             //Change the random value for height
-                array[i] = Math.floor(Math.random()*750)
+                array[i] = i
         }
+        console.log("Array: ",array);
+        
+        let arr = []
+        let init = array.length
+        for(let j = 0; j < init; j++){
+            let r = Math.floor(Math.random()*array.length)
+            arr.push(array[r])
+            array.splice(r,1)
+        }
+        console.log("Arr: ", arr);
+        
+        array = arr
         return array
+    }
+
+    mergeSort() {
+        let list = this.state.randomArray
+        const len = list.length
+        // an array of length == 1 is technically a sorted list
+        if (len == 1) {
+            return list
+        }
+    
+        // get mid item
+        const middleIndex = Math.ceil(len / 2)
+    
+        // split current list into two: left and right list
+        let leftList = list.slice(0, middleIndex)
+        let rightList = list.slice(middleIndex, len)
+    
+        list = this.merge(leftList, rightList)
+        this.setState({ randomArray: list })
+        return
+    }
+    
+      // Solve the sub-problems and merge them together
+    merge(leftList, rightList) {
+        const sorted = []
+        while (leftList.length > 0 && rightList.length > 0) {
+            const leftItem = leftList[0]
+            const rightItem = rightList[0]
+            if (leftItem > rightItem) {
+                sorted.push(rightItem)
+                rightList.shift()
+            } else { 
+                sorted.push(leftItem);
+                leftList.shift()
+            }
+        }
+    
+        // if left list has items, add what is left to the results
+        while (leftList.length !== 0) {
+            sorted.push(leftList[0])
+            leftList.shift()
+        }
+    
+        // if right list has items, add what is left to the results
+        while (rightList.length !== 0) {
+            sorted.push(rightList[0])
+            rightList.shift()
+        }
+    
+        // merge the left and right list
+        return sorted
+    }
+
+    async mergeLoop() {
+        for(let i = 0; i < 20; i++){
+            await this.sleep(10);
+            this.mergeSort()
+        }
     }
 
     bubbleSort() {
@@ -46,14 +116,15 @@ class Visualizer extends Component {
     }
     
     render(){
-        let boxes = this.state.randomArray.map(value => <Square value = {value}/>)
+        let boxes = this.state.randomArray.map(value => <Square value = {value}
+        length = {this.state.randomArray.length}/>)
         return (
             <div>
                 <div className = "align">
                     { boxes }
                 </div>
                 <button onClick={() => this.bubbleLoop()}>Bubble Sort</button>
-                {/* <button onClick={() => this.selectionLoop()}>Selection Sort</button> */}
+                <button onClick={() => this.mergeLoop()}>Merge Sort</button>
             </div>
         );
     }
